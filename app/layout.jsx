aa4695274405d2a449c02c6fd84e68527d304e81
=elangyudha280 <=elangyudharakasiwi2@gmail.com>
2024-07-19 23:09:34 -0700

    selesai buat button action navbar

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,7 +3,7 @@ import Head from "next/head";
 import "./globals.css";
 const epilogue = Epilogue({ 
   subsets: ["latin"],
-  weight:['400','500','600']
+  weight:['400','500','600','700']
 });
 
 export const metadata = {
--- components/navbar/navbar.jsx
+++ components/navbar/navbar.jsx
@@ -1,10 +1,38 @@
+import Link from "next/link";
 
 
 
 export function Navbar(){
     return (
-        <nav className="fixed top-0 left-0 w-full h-[50px] border-2 border-red-500">
+        <nav className="relative top-0 left-0 w-full px-2 h-[55px] py-2 border-2 border-red-500">
+            {/*//! Container nav */}
+            <section className="max-w-6xl h-full  mx-auto flex gap-x-4 ">
+                {/* title */}
+                <Link href='/' className="font-bold h-full flex items-center text-[2em]">
+                    snap
+                </Link>
+                {/* nav container items */}
+                <section className="nav_bg_dark relative flex-1 h-full ">
+                    <div className="_nav_items_container relative w-full h-full flex gap-1">
+                        
+                        {/* nav items */}
+                        <section className="flex-1 w-full h-full flex gap-2   px-2 border-2 border-red-500">
 
+                        </section>
+                        {/* button action */}
+                        <section className="action_button h-full flex px-2 gap-3">
+                            {/* button login */}
+                            <button className="btn_auth hover:text-custom-almost-black">
+                                Login
+                            </button>
+                            {/* button regiter */}
+                            <button className="btn_auth ring-2 ring-custom-medium-gray hover:ring-custom-almost-black hover:text-custom-almost-black">
+                                Register
+                            </button>
+                        </section>
+                    </div>
+                </section>
+            </section>
         </nav>
     )
 }
