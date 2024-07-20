e15670e8637310ad5e980150dc2e3d325bcb8bb2
=elangyudha280 <=elangyudharakasiwi2@gmail.com>
2024-07-19 22:45:41 -0700

    Selesai buat component error

---
+++ app/error.jsx
@@ -0,0 +1,15 @@
+'use client'
+
+import React from 'react'
+
+function ErrorGlobal() {
+  return (
+    <section className="relative w-full h-[100vh] p-2 overflow-x-hidden bg-custom-almost-white grid place-items-center">
+        <h2 className="text-center">
+            oops something wrong try again later....
+        </h2>
+    </section>
+  )
+}
+
+export default ErrorGlobal
--- app/page.jsx
+++ app/page.jsx
@@ -4,7 +4,6 @@ export default function Home() {
   return (
     <main className="relative w-full h-[100vh] overflow-x-hidden overflow-y-auto bg-custom-almost-white">
       <section className="relative w-full h-full bg-custom-almost-white">
-
       </section>
     </main>
   );
