a3c20d602a93c61bfae259593e9aad073530990d
=elangyudha280 <=elangyudharakasiwi2@gmail.com>
2024-07-19 22:42:10 -0700

    Selesai buat page loading

--- app/page.js
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-
-export default function Home() {
-  return (
-    <main className="relative w-full h-[100vh] overflow-x-hidden overflow-y-auto">
-      
-    </main>
-  );
-}
---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="relative w-full h-[100vh] overflow-x-hidden overflow-y-auto bg-custom-almost-white">
+      <section className="relative w-full h-full bg-custom-almost-white">
+
+      </section>
+    </main>
+  );
+}
