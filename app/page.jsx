95cfe61b99ee256c53bc93acf55fef5bca45fadd
=elangyudha280 <=elangyudharakasiwi2@gmail.com>
2024-07-19 22:50:18 -0700

    buat page navbar

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,14 @@
 import Image from "next/image";
 
+// import component
+import { Navbar } from "@/components/navbar/navbar";
+
 export default function Home() {
   return (
     <main className="relative w-full h-[100vh] overflow-x-hidden overflow-y-auto bg-custom-almost-white">
       <section className="relative w-full h-full bg-custom-almost-white">
+        {/*//! NAVBAR */}
+        <Navbar/>
       </section>
     </main>
   );
--- components/navbar/navbar.jsx
+++ components/navbar/navbar.jsx
@@ -3,7 +3,7 @@
 
 export function Navbar(){
     return (
-        <nav className="fixed top-0 left-0 w-full border-2 border-red-500">
+        <nav className="fixed top-0 left-0 w-full h-[50px] border-2 border-red-500">
 
         </nav>
     )
